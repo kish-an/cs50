90846372c7238158fab6548c13e5883bab4a6725
Kishan <kishan.o@hotmail.com>
2020-01-31 22:12:28 +0000

    Update 9.sql to match spec

--- pset7/movies/9.sql
+++ pset7/movies/9.sql
@@ -1,7 +1,9 @@
--- DISTINCT removes duplicates
-SELECT DISTINCT people.name
+SELECT people.name
 FROM people
-JOIN stars ON people.id = stars.person_id
-JOIN movies on stars.movie_id = movies.id
-WHERE movies.year = 2004
+WHERE people.id IN
+-- DISTINCT removes duplicates (need to remove duplicates at stars id as different people may have same name)
+(SELECT DISTINCT stars.person_id
+FROM stars
+JOIN movies ON stars.movie_id = movies.id
+WHERE movies.year = 2004)
 ORDER BY people.birth;
